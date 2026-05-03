3dbde92775999451e028a534305e11de70054bef
yulmoo75 <yulmoo0705@gmail.com>
2026-05-03 21:26:08 +0900

    [0503] 천유진

--- everytime/everytime_prj/accounts/urls.py
+++ everytime/everytime_prj/accounts/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', mypage, name='mypage'),
     path('user-info/', user_info, name='user-info'),
     path('mypage/', mypage, name='mypage'), 
+    path('mypost/', mypost, name='mypost')
 ]
