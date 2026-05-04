47ddc2fa83b4aa5b3b3bbae519ccd7482994274f
Seo yeon Kim <merock2tar@gmail.com>
2026-05-04 21:24:57 +0900

    [0504] 김서연 7차 세션

--- blog/blog_prj/blog/urls.py
+++ blog/blog_prj/blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('create/', create, name='create'),
     path('detail/<int:id>/', detail, name='detail'), 
     path('update/<int:id>/', update, name='update'),
-    path('delete/<int:id>/', delate, name='delete'),
+    path('delete/<int:id>/', delete, name='delete'),
     path('create-comment/<int:post_id>/', create_comment, name='create-comment'),
+    path('like/<int:post_id>/', like, name='like'),
 ]
